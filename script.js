cbfaec05f1030590f4a026a8800d20a166a319f6
David Griswold <novachild@gmail.com>
2026-05-03 13:38:06 +0300

    added pause button

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 let countdown;
+let paused = false;
 let time = 90;
 let count = time;
+let pauseButton = document.getElementById("pauseButton");
+let faceImg = document.getElementById("face");
+let journal = document.getElementById("journal");
 
-setInterval(go, time * 1000);
+faceImg.addEventListener("animationstart", () => {
+    journal.classList.remove("blink");
+});
+faceImg.addEventListener("animationend", () => {
+    journal.classList.add("blink");
+});
+
+pauseButton.addEventListener("click", () => {
+    clearInterval(countdown);
+    pauseButton.innerHTML = "▶";
+    journal.classList.remove("blink");
+    if (paused) {
+        clearInterval(countdown);
+        countdown = setInterval(go, 1000);
+        pauseButton.innerHTML = "⏸";
+        journal.classList.add("blink");
+    }
+    paused = !paused;
+});
+
+countdown = setInterval(go, 1000);
 go();
 
 function go() {
-    console.log("playing");
-    let sound = document.getElementById("sound");
-    let face = document.getElementById("face");
     let countdownText = document.getElementById("countdown");
-    sound.play();
-    face.classList.remove("go");
-    face.offsetWidth;
-    face.classList.add("go");
-    clearInterval(countdown);
-    count = time;
+    count--;
+    if (count <= 0) {
+        console.log("playing");
+        let sound = document.getElementById("sound");
+        let face = document.getElementById("face");
+        sound.play();
+        face.classList.remove("go");
+        face.offsetWidth;
+        face.classList.add("go");
+        count = time;
+    }
     countdownText.innerText = count;
-    countdown = setInterval(() => {
-        count--;
-        countdownText.innerText = count;
-    }, 1000);
 }
 
-document.getElementById("face").addEventListener("animationstart", () => {
-    document.getElementById("journal").classList.remove("blink");
-});
-document.getElementById("face").addEventListener("animationend", () => {
-    document.getElementById("journal").classList.add("blink");
-});
