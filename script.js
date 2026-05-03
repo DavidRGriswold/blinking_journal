bc323f59b1d28fc300235af02a5268ae7994a429
David Griswold <novachild@gmail.com>
2026-05-03 13:07:03 +0300

    move controls to the bottom, add countdown

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-setInterval(() => {
+let countdown;
+let time = 90;
+let count = time;
+
+setInterval(go, time * 1000);
+go();
+
+function go() {
     console.log("playing");
     let sound = document.getElementById("sound");
     let face = document.getElementById("face");
+    let countdownText = document.getElementById("countdown");
     sound.play();
     face.classList.remove("go");
     face.offsetWidth;
     face.classList.add("go");
-}, 90000);
+    clearInterval(countdown);
+    count = time;
+    countdownText.innerText = count;
+    countdown = setInterval(() => {
+        count--;
+        countdownText.innerText = count;
+    }, 1000);
+}
 
 document.getElementById("face").addEventListener("animationstart", () => {
     document.getElementById("journal").classList.remove("blink");
